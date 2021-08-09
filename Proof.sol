48bd764c7cc55229972647f83d9d2989fd4100fe
Bouichou A <34541776+elicha1@users.noreply.github.com>
2021-08-09 09:19:14 +0100

    Add files via upload

--- Proof.sol
+++ Proof.sol
@@ -10,14 +10,19 @@ contract Proof{
        }
       mapping(string=>FileDetails)files;
       event logFileAddedStatus(bool status, uint timestamp, string owner,string fleHash);
+      //store the owner of file at the block timestamp 
       function set( string owner, string fileHash){
                 if (files[fileHash].timestamp==0){
+                // needs more reflexion to check if a key is already taken 
                    files[fileHash]= FileDetails(block.timestamp,owner);
+                   // trigger event so the frontend knows about it
                    logFileAddedStatus(true,block.timestamp,owner,fileHash);
                 }else{
+                //tell frontend that file exists and owned already 
                     logFileAddedStatus(false,block.timestamp,owner,fileHash);
                 }
        }
+       // get file informations
        function get(string fileHash)returns(uint timestamp, string owner){
                 return(files[fileHash].timestamp, files[fileHash].owner);
         }
